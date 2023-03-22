ee6e97fbc79c1ccdac814f7a09c717ed655057b0
yasminaa12 <yasminabendriss1@gmail.com>
2023-03-21 19:46:09 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,22 @@
 #include "main.h"
+
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * print_alphabet_x10 - Entry point
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
 {
-print_alphabet_x10();
-return (0);
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
