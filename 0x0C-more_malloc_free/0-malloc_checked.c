10e0f47b09714a5810239bb42346bd29926a2ac2
yasminaa12 <yasminabendriss1@gmail.com>
2023-04-13 16:23:26 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -15,3 +15,4 @@ void *malloc_checked(unsigned int b)
 		exit(98);
 	return (i);
 }
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,13 +2,54 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+ * error_exit - prints error with _putchar
+ * Return: 98
+ */
+int error_exit(void)
+{
+	char *err;
+	int i;
 
-#endif
+	err = "Error";
+	for (i = 0; err[i] != '\0'; i++)
+		_putchar(err[i]);
+	_putchar('\n');
+	exit(98);
+}
+/**
+ * check_number - cheks for num
+ * @str: string
+ * Return: 0 or 1
+ */
+int check_number(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+/**
+ * _length - length of str
+ * @str: string
+ * Return: length of str
+ */
+int _length(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
