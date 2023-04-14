16b664507b5c8e33d47dd756385fd9d2ee83108e
yasminaa12 <yasminabendriss1@gmail.com>
2023-04-14 15:22:53 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -76,11 +76,11 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (len2 = len2 - 1; len2 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len1 = len - 1; len1 >= 0; len2--)
+		for (len2 = len2 - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
