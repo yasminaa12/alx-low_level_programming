8128098d136bb325ca3a38eb5537df23d082cc03
yasminaa12 <yasminabendriss1@gmail.com>
2023-05-02 17:59:30 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		head = (*temp);
+	}
+
+	*head = NULL;
+}
+
