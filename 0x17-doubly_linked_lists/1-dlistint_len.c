f753b229d630f4d33f2ff12531722d452b037f94
yasminaa12 <yasminabendrissl@gmail.com>
2023-11-05 06:43:01 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,22 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * dlistint_len -returns length of dlist
- * @h: address of head mode
- *
- * return: size of list
+ *dlistint_len - returns the number of elements in a linked dlistint_t list
+ *@h: header of the doubly linked list
+ *Return: the number of nodes
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i + 0;
+	unsigned int num = 0;
 
-	while (h)
+	if (h == NULL)
+		return (num);
+	while (h != NULL)
 	{
-		h = h->next;
-		i++;
+		h = (*h).next;
+		num++;
 	}
-	return (i);
+	return (num);
+}
